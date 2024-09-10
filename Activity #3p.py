874cdde42ebcfa4f68bac13c5f40a592ff775c19
Renzo Emmanuel Ramos <143341990+RenzoEmamnuel@users.noreply.github.com>
2024-09-10 21:56:57 +0800

    Update Activity #3p.py

--- Activity #3p.py
+++ Activity #3p.py
@@ -1,34 +1,47 @@
 from tabulate import tabulate
 
-fare_rate = {
-    "jeepney": [12, 10, 9.50],
-    "tricycle": [50, 40, 38],
-    "bus": [10, 12, 9.50]
-}
 class PUV:
-
-    def __init__(self, vehicle):
+    def __init__(self, vehicle: str, regular_rate: float, student_rate: float, senior_rate: float):
         self.vehicle = vehicle
-        if vehicle in fare_rate:
-            headers = ["Category", "Fare (₱)"]
-            data = [
-                ["Regular", fare_rate[vehicle][0]],
-                ["Student", fare_rate[vehicle][1]],
-                ["Senior Citizen", fare_rate[vehicle][2]]
-            ]
-            print(f"\nFare rate for {vehicle.capitalize()}:")
-            print(tabulate(data, headers, tablefmt="rounded_grid",colalign=("center", "center")))
-        else:
-            print("!!!!!!! Invalid Input !!!!!!!!!!!")
+        self.regular_rate = regular_rate
+        self.student_rate = student_rate
+        self.senior_rate = senior_rate
 
-while True:
-    print("----------------------- List of vehicles -----------------------\n"
-          "                         ↓↓↓↓↓↓↓↓↓↓↓↓↓↓\n"
-          "                          -> Jeepney\n"
-          "                          -> Tricycle\n"
-          "                          -> Bus")
-    choose_vehicle = input("What vehicle do you want to ride? (or type [x] to quit): ").lower()
-    if choose_vehicle == 'x':
-        print("Exiting the Vehicle fare checker. Have a nice day!")
-        break
-    PUV(choose_vehicle)
+    def show_price(self):
+        headers = ["Category", "Fare (₱)"]
+        data = [
+            ["Regular", self.regular_rate],
+            ["Student", self.student_rate],
+            ["Senior Citizen", self.senior_rate]
+        ]
+        print(f"\nFare rate for {self.vehicle.capitalize()}:")
+        print(tabulate(data, headers, tablefmt="rounded_grid", colalign=("center", "center")))
+
+def main():
+    jeepney = PUV("jeepney", 12, 10, 9.50)
+    tricycle = PUV("tricycle", 50, 40, 38)
+    bus = PUV("bus", 12, 10, 9.50)
+
+    while True:
+        print("----------------------- List of vehicles -----------------------\n"
+              "                         ↓↓↓↓↓↓↓↓↓↓↓↓↓↓\n"
+              "                          -> Jeepney\n"
+              "                          -> Tricycle\n"
+              "                          -> Bus")
+        choose_vehicle = input("What vehicle do you want to ride? (or type [x] to quit): ").lower()
+
+        if choose_vehicle in ["jeepney", "tricycle", "bus"]:
+            if choose_vehicle == "jeepney":
+                jeepney.show_price()
+            elif choose_vehicle == "tricycle":
+                tricycle.show_price()
+            elif choose_vehicle == "bus":
+                bus.show_price()
+        elif choose_vehicle == 'x':
+            print("Exiting the Vehicle fare checker. Have a nice day!")
+            break
+        else:
+            print("Invalid Input. Please select a valid vehicle from the list.")
+
+if __name__ == "__main__":
+    main()
